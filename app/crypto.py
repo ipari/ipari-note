694cb01776e91a0d90a6419790041d1faa565c9d
ipari <me@ipari.dev>
2020-03-16 20:45:01 +0900

    AES 암호화, 복호화 추가

---
+++ app/crypto.py
@@ -0,0 +1,40 @@
+import base64
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from app.utils import config
+
+PADDING = '$'
+AES_KEY_LENGTH = 32
+
+
+def get_aes_key():
+    key = config('secret.key')
+    if len(key) > AES_KEY_LENGTH:
+        key = key[:AES_KEY_LENGTH]
+    if len(key) < AES_KEY_LENGTH:
+        key += (AES_KEY_LENGTH - len(key)) * PADDING
+    return key.encode()
+
+
+def get_secret():
+    key = get_aes_key()
+    iv = key[::2][::-1]
+    return key, iv
+
+
+def encrypt(text):
+    key, iv = get_secret()
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    encrypted = cipher.encrypt(pad(text.encode(), AES.block_size))
+    return base64.urlsafe_b64encode(encrypted).decode('utf-8')
+
+
+def decrypt(encrypted):
+    key, iv = get_secret()
+    try:
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        encrypted = base64.urlsafe_b64decode(encrypted)
+        decrypted = cipher.decrypt(encrypted)
+        return unpad(decrypted, AES.block_size).decode('utf-8')
+    except ValueError:
+        return None
