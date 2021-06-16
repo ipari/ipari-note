6bd245cd51d14b4d2b6948705febbb5d5bcc85d8
ipari <me@ipari.dev>
2021-06-16 17:47:59 +0900

    태그에 속한 페이지 목록 출력

--- app/main/view.py
+++ app/main/view.py
@@ -14,7 +14,7 @@ POST_PER_PAGE = 5
 
 @bp.route('/')
 def view_index():
-    posts, next_url, prev_url = get_page(page=1)
+    posts, next_url, prev_url = get_page()
     return render_template('posts.html',
                            meta=get_base_meta(),
                            pagename='',
@@ -34,26 +34,47 @@ def view_posts(page):
                            prev_url=prev_url)
 
 
+@bp.route('/tags')
+def view_tags():
+    from sqlalchemy import func
+    tag_info = Tag.query.with_entities(Tag.tag, func.count(Tag.note_id))\
+        .group_by(Tag.tag)\
+        .order_by(func.count(Tag.note_id).desc(), Tag.tag)\
+        .all()
+    return tag_info
+
+
+@bp.route('/tags/<tag>')
+def view_tag(tag):
+    posts, next_url, prev_url = get_tag_page(tag)
+    return render_template('posts.html',
+                           meta=get_base_meta(),
+                           pagename='',
+                           posts=posts,
+                           next_url=next_url,
+                           prev_url=prev_url)
+
+
+@bp.route('/tags/<tag>/<int:page>')
+def view_tag_posts(tag, page):
+    posts, next_url, prev_url = get_tag_page(tag, page=page)
+    return render_template('posts.html',
+                           meta=get_base_meta(),
+                           pagename='',
+                           posts=posts,
+                           next_url=next_url,
+                           prev_url=prev_url)
+
+
 @bp.route('/update')
 def view_update():
     update_all()
     return 'update'
 
 
-def get_page(page=0):
-    base_query = Note.query.filter_by(permission=Permission.PUBLIC).\
-        order_by(Note.pinned.desc(), Note.updated.desc())
-    page = base_query.paginate(page, POST_PER_PAGE, False)
-
-    next_url = None
-    prev_url = None
-    if page.next_num:
-        next_url = url_for('main.view_posts', page=page.next_num)
-    if page.prev_num:
-        prev_url = url_for('main.view_posts', page=page.prev_num)
-
+def get_post_info_from_notes(list_of_note):
     posts = []
-    for item in page.items:
+    for item in list_of_note:
         post = {
             'created': item.created,
             'updated': item.updated,
@@ -65,5 +86,37 @@ def get_page(page=0):
             'title': item.title,
         }
         posts.append(post)
+    return posts
 
+
+def get_page(page=1):
+    base_query = Note.query.filter_by(permission=Permission.PUBLIC).\
+        order_by(Note.pinned.desc(), Note.updated.desc())
+    page = base_query.paginate(page, POST_PER_PAGE, False)
+
+    next_url = None
+    prev_url = None
+    if page.next_num:
+        next_url = url_for('main.view_posts', page=page.next_num)
+    if page.prev_num:
+        prev_url = url_for('main.view_posts', page=page.prev_num)
+
+    posts = get_post_info_from_notes(page.items)
+    return posts, next_url, prev_url
+
+
+def get_tag_page(tag, page=1):
+    base_query = Note.query.join(Tag, Note.id == Tag.note_id)\
+        .filter(Tag.tag == tag, Note.permission == Permission.PUBLIC)\
+        .order_by(Note.pinned.desc(), Note.updated.desc())
+    page = base_query.paginate(page, POST_PER_PAGE, False)
+
+    next_url = None
+    prev_url = None
+    if page.next_num:
+        next_url = url_for('main.view_tag_posts', tag=tag, page=page.next_num)
+    if page.prev_num:
+        prev_url = url_for('main.view_tag_posts', tag=tag, page=page.prev_num)
+
+    posts = get_post_info_from_notes(page.items)
     return posts, next_url, prev_url
