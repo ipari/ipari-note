a90fe27357f0a5252c0d2afb5bae8d48a9fbb720
ipari <me@ipari.dev>
2026-06-24 22:41:53 +0900

    Add configurable watchdog observer

--- app/watcher.py
+++ app/watcher.py
@@ -2,6 +2,7 @@ import os
 import time
 import traceback
 from watchdog.observers import Observer
+from watchdog.observers.polling import PollingObserver
 from watchdog.events import FileSystemEventHandler
 
 from app.config.model import CONFIG_PATH
@@ -10,6 +11,8 @@ from app.user.model import USER_PATH
 
 MARKDOWN_EXT = '.md'
 PAGE_PATH = os.path.join('data', 'pages')
+DEFAULT_WATCHDOG_OBSERVER = 'native'
+WATCHDOG_OBSERVER_MODES = ('native', 'polling')
 
 
 class PageWatcher(object):
@@ -17,7 +20,10 @@ class PageWatcher(object):
     event_order = ['created', 'modified', 'moved', 'deleted']
 
     def __init__(self):
-        self.observer = Observer()
+        self.page_path = os.path.realpath(PAGE_PATH)
+        self.user_path = os.path.realpath(USER_PATH)
+        self.config_path = os.path.realpath(CONFIG_PATH)
+        self.observer = create_observer()
         self.page_handler = EventHandler()
         self.user_handler = EventHandler()
         self.config_handler = EventHandler()
@@ -25,11 +31,11 @@ class PageWatcher(object):
     def watch(self):
         self.observer.schedule(
             self.page_handler,
-            os.path.realpath(PAGE_PATH),
+            self.page_path,
             recursive=True,
         )
-        self.observer.schedule(self.user_handler, os.path.realpath(USER_PATH))
-        self.observer.schedule(self.config_handler, os.path.realpath(CONFIG_PATH))
+        self.observer.schedule(self.user_handler, self.user_path)
+        self.observer.schedule(self.config_handler, self.config_path)
         self.observer.start()
 
         try:
@@ -102,6 +108,26 @@ class EventHandler(FileSystemEventHandler):
         if event.is_directory:
             return
         self.buffer.append(event)
+
+
+def create_observer():
+    mode = get_observer_mode()
+    if mode == 'polling':
+        return PollingObserver()
+    return Observer()
+
+
+def get_observer_mode():
+    try:
+        from config import WATCHDOG_OBSERVER
+        mode = WATCHDOG_OBSERVER
+    except ImportError:
+        mode = DEFAULT_WATCHDOG_OBSERVER
+
+    mode = str(mode).lower()
+    if mode not in WATCHDOG_OBSERVER_MODES:
+        return DEFAULT_WATCHDOG_OBSERVER
+    return mode
 
 
 if __name__ == '__main__':
